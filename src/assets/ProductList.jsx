4d1e40d5aa9454fe0ecd330be9997e7371d21b8e
maurizio-dominici <mauriziodominici4@gmail.com>
2025-09-24 16:17:49 +0200

    add milestone 2

--- src/assets/ProductList.jsx
+++ src/assets/ProductList.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import products from "../js/products";
 
 export default function ProductList() {
-  console.log(products);
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  const addToCart = (product) => {
+    const isProductAlreadyAdded = addedProducts.some(
+      (p) => p.name === product.name
+    );
+    if (isProductAlreadyAdded) {
+      return;
+    }
+    const productToAdd = {
+      ...product,
+      quantity: 1,
+    };
+    setAddedProducts((curr) => [...curr, productToAdd]);
+  };
 
   return (
     <>
@@ -14,11 +30,38 @@ export default function ProductList() {
               <li key={index}>
                 <strong>Nome :</strong>
                 {product.name} / <strong>Prezzo :</strong>
-                {product.price}
+                {product.price.toFixed(2)}
+                <button onClick={() => addToCart(product)}>
+                  Aggiungi al carrello
+                </button>
               </li>
             );
           })}
         </ul>
+        {addedProducts.length > 0 && (
+          <>
+            <h2>Carrello</h2>
+
+            <ul>
+              {addedProducts.map((addp) => (
+                <li>
+                  <p>
+                    <strong>Nome :</strong>
+                    {addp.name}
+                  </p>
+                  <p>
+                    <strong>Prezzo :</strong>
+                    {addp.price.toFixed(2)}
+                  </p>
+                  <p>
+                    <strong>Quantità :</strong>
+                    {addp.quantity}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </>
   );
